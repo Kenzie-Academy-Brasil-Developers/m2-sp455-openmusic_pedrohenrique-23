413f760c85c231fc62194009a20cac462a7a267a
pedrohenrique-23 <pedrosilva10057@gmail.com>
2023-08-24 14:49:06 -0300

    Tarefa 2 | Aplicando os Filtros

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,7 @@ import { products, categories } from "./productsData.js";
 
 function createCard(product){
     const card = document.createElement("li")
+    card.classList.add("card");
     /* Trabalhe sua lógica aqui */
     const imgCard = document.createElement("img");
   imgCard.src = product.img; // Set the image source
@@ -35,6 +36,7 @@ function renderFilterButtons(array) {
       const listItem = document.createElement("li");
       const filterButton = document.createElement("button");
       filterButton.innerText = category;
+      filterButton.classList.add("list-filters_button")
       listItem.appendChild(filterButton);
       listFilters.appendChild(listItem);
     });
@@ -42,12 +44,66 @@ function renderFilterButtons(array) {
 
   function renderCards(array) {
     const listCards = document.querySelector(".list-cards");
+    
+    while (listCards.firstChild) {
+      listCards.removeChild(listCards.firstChild);
+    }
+
     array.forEach(product => {
       const card = createCard(product);
       listCards.appendChild(card);
     });
   }
   
-  // Call the functions to render the components
   renderFilterButtons(categories);
   renderCards(products);
+
+  // TAREFA 2
+
+  function addEventListenersToButtons(categories, products) {
+    const filterButtons = document.querySelectorAll(".list-filters_button");
+  
+    filterButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        const buttonText = button.innerText;
+        const categoryIndex = categories.indexOf(buttonText);
+  
+        if (categoryIndex === 0) {
+          renderCards(products);
+          // console.log("Todos os generos");
+        } else {
+          const filteredProducts = products.filter(product => product.category === categoryIndex);
+          // console.log("Genero filtrado:", filteredProducts);
+          renderCards(filteredProducts);
+        }
+      });
+    });
+  
+    const priceInput = document.querySelector(".input-range");
+    const priceParagraph = document.querySelector(".container__price p");
+  
+    priceInput.addEventListener("input", () => {
+      const selectedPrice = parseFloat(priceInput.value);
+      priceParagraph.innerText = `Até R$ ${selectedPrice.toFixed(2)}`;
+      const filteredProducts = products.filter(product => product.price <= selectedPrice);
+
+      const allCards = document.querySelectorAll(".card");
+  allCards.forEach(card => {
+    card.classList.remove("hidden");
+  });
+
+  // Adicione a classe "hidden" apenas aos cards que não fazem parte da filtragem
+  allCards.forEach(card => {
+    if (!filteredProducts.some(product => product.id.toString() === card.dataset.productId)) {
+      card.classList.add("hidden");
+    }
+  });
+
+      renderCards(filteredProducts);
+    });
+  }
+  
+  // Chamar a função addEventListenersToButtons passando os arrays de categorias e produtos
+  addEventListenersToButtons(categories, products);
+  
+  
